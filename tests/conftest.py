48b037f2ecf04e6bd5663edd8c364c8491fa9cb8
Kambarov Kuanysh <kambarovkuanysh@gmail.com>
2026-04-04 01:26:06 +0500

    overall architecture sync commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,6 @@
 import pytest
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
 
-@pytest.fixture(scope="function")
-def driver():
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")  # Run without a UI
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    
-    # Automatically manages the chromedriver version
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=chrome_options)
-    
-    yield driver
-    driver.quit()
+
+@pytest.fixture(scope="session")
+def base_url():
+    return "http://localhost:3001"
